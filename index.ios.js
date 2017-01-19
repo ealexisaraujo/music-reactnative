67c791a2b5e4e4893a590ef9132f2b0353e69267
Alexis Araujo <ala1289@gmail.com>
2017-01-19 13:49:21 -0600

    Components and API´s

--- index.ios.js
+++ index.ios.js
@@ -4,21 +4,43 @@
  * @flow
  */
 
+// importar modulos externos o que se hayan declaro
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  Alert
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
+  onPress() {
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ]
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Platzi Music!
         </Text>
+        <Button
+          onPress={this.onPress}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
