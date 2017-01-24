97ec8f6f9195ad306dbbba32f6ba1ce37faab267
Alexis Araujo <ala1289@gmail.com>
2017-01-24 15:25:18 -0600

    Create ArtistBox

--- app.js
+++ app.js
@@ -12,20 +12,38 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
-  Alert
+  Image
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class AwesomeProject extends Component {
   
-  
-
   render() {
+    const image = 'https://static.platzi.com/media/files/bowie_a927fdf3-b321-4a5c-99ca-239cc86c57bc.png'
+    const name = 'David Bowie'
+    const likes = 200
+    const comments = 140
+    
     return (
       <View style={styles.container}>
-        
-
+        <View style={styles.artistBox}>
+        <Image style={styles.image} source={{uri : image }}/>
+        <View style={styles.info}>
+          <Text style={styles.name}>{name}</Text>
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Icon name="ios-heart-outline" size={30} color="gray"/>
+              <Text style={styles.count}>{likes}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Icon name="ios-chatboxes-outline" size={30} color="gray"/>
+              <Text style={styles.count}>{comments}</Text>
+            </View>
+          </View>
+          
+        </View>
+        </View>
       </View>
     );
   }
@@ -34,9 +52,40 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'lightgray',
+    paddingTop: 50,
+  },
+  artistBox:{
+    backgroundColor: 'white',
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    backgroundColor: 'red',
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+  info:{
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    color: '#333'
+  },
+  row: {
+    flexDirection : 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+    marginTop: 15,
+  },
+  iconContainer: {
+    flex : 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: 'gray'
   }
 });
 
