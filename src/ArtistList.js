4ded6d57de11117713d1671aada66490a6bfd035
Alexis Araujo <ala1289@gmail.com>
2017-01-31 14:07:32 -0600

    Contruction artislist.js

---
+++ src/ArtistList.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+// importar modulos externos o que se hayan declaro
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  ListView,
+} from 'react-native';
+
+import ArtistBox from './ArtistBox'
+
+export default class ArtisList extends Component {
+
+  constructor(props){
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    
+    this.state = {
+      dataSource: ds.cloneWithRows(props.artists),
+    };
+  }
+  
+  render() {
+  
+
+    return (
+      <ListView
+      dataSource={this.state.dataSource}
+      renderRow={(artist) => <ArtistBox artist={artist}/>}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    paddingTop: 50,
+  },
+});
--- src/app.js
+++ src/app.js
@@ -10,33 +10,26 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
-  Image,
-  ScrollView,
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import ArtistBox from './ArtistBox'
+import ArtisList from './ArtistList.js'
 
 export default class AwesomeProject extends Component {
-  
+
   render() {
-  const artist = {
+        const artist = {
       image : 'https://static.platzi.com/media/files/bowie_a927fdf3-b321-4a5c-99ca-239cc86c57bc.png',
       name : 'David Bowie',
       likes : 200,
       comments : 140,
     }
+    const artists = Array(500).fill(artist);
 
     return (
-      <ScrollView style={styles.container}>
-        {
-          Array(10).fill(artist).map(artist => {
-            return <ArtistBox artist={artist}/>
-          })
-        }  
-      </ScrollView>
+      <View style={styles.container}>
+      <ArtisList    artists={artists}/>
+      </View>
     );
   }
 }
