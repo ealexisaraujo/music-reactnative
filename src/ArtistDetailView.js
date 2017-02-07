9f12aebf513d2d9c920b5ec80e431590bfaa8516
Alexis Araujo <ala1289@gmail.com>
2017-02-07 16:41:39 -0600

    Functionality Comments Artist

--- src/ArtistDetailView.js
+++ src/ArtistDetailView.js
@@ -6,22 +6,52 @@
 
 // importar modulos externos o que se hayan declaro
 
+import KeyboardSpacer from 'react-native-keyboard-spacer'; 
 import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
 import ArtistBox from './ArtistBox'
 import { getArtist } from './api-client.js'
+import {firebaseDatabase, firebaseAuth} from './firebase'
 
 export default class ArtisDetailView extends Component {
+hanldeSend = () => {
+  const {text} = this.state
+  const artistCommentRef = this.getArtistComomentsRef()
+  var newCommentRef = artistCommentRef.push();
+  newCommentRef.set({text});
+}
+
+  getArtistComomentsRef = () => {
+    const {id} = this.props.artist
+    return firebaseDatabase.ref(`comments/${id}`)
+  }
+
+handleChangeText = (text) => this.setState({text})
+
+
  render() {
     const artist = this.props.artist
 
     return (
       <View style={styles.container}>
       <ArtistBox    artist={artist}/>
+      <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Opina sobre este artista"
+            onChangeText={this.handleChangeText}
+          />
+          <TouchableOpacity onPress={this.hanldeSend}> 
+               <Icon name="ios-send-outline" size={30} color="gray"/>
+          </TouchableOpacity> 
+        </View>
       </View>
     );
   }
@@ -33,6 +63,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
     paddingTop: 70,
   },
+  inputContainer:{
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    height: 50,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    height: 50,
+    flex: 1
+  }
 });
 
 
